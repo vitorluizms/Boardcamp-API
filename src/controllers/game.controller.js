d5607602444267e174157334057d1d39392024e6
vitorluizms <vitor.luiz.eer@gmail.com>
2023-07-29 13:17:36 -0300

    feat: Adicionar jogo ao banco de dados adicionado

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -3,12 +3,27 @@ import db from "../database/database.connection.js";
 export async function getGames(req, res) {
   try {
     const answer = await db.query(`SELECT * FROM games`);
-    res.send(answer.rows);
+    res.send(answer);
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
 export async function postGame(req, res) {
-  res.send("okay")
+  const { name, image, stockTotal, pricePerDay } = req.body;
+  try {
+    const validation = await db.query(`SELECT * FROM games WHERE name = $1`, [
+      name,
+    ]);
+    if (validation.rowCount > 0) {
+      return res.status(409).send("Game already exists");
+    }
+    await db.query(
+      `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`,
+      [name, image, stockTotal, pricePerDay]
+    );
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
--- src/schemas/main.schemas.js
+++ src/schemas/main.schemas.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const gameSchema = joi.object({
   name: joi.string().required(),
-  string: joi.string().required(),
+  image: joi.string().required(),
   stockTotal: joi.number().min(1).required(),
   pricePerDay: joi.number().min(1).required(),
 });
