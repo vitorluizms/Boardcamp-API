37228db3c0c3e062ea95406a694aa73804cddce3
vitorluizms <vitor.luiz.eer@gmail.com>
2023-07-29 22:30:21 -0300

    fix: Correção na função de listar clientes

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -1,11 +1,12 @@
-import db from "../database/database.connection";
+import db from "../database/database.connection.js";
 
 export async function getCustomers(req, res) {
-    try {
-        const customers = await db.query(`SELECT * FROM customers`)
-        res.status(200).send(customers.rows)
-    }
-    catch (err) {
-        res.status(500).send(err.message)
-    }
+  try {
+    const customers = await db.query(
+      `SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY - MM - DD') AS birthday FROM customers`
+    );
+    res.status(200).send(customers.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import gameRouter from "./games.routes.js";
+import customersRouter from "./clients.routes.js";
 
 const router = Router();
 
 router.use(gameRouter);
+router.use(customersRouter);
 
 export default router;
